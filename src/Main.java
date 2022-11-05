094896baa5534201cdbc301faeadfe35d8b41c88
BoomStickk <6endgame6@gmail.com>
2022-11-05 11:24:51 +0200

    works if i call method choosePokemon directly in main,

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,25 @@ public class Main {
         FirePokemon fire2=new FirePokemon("Ninetails","medium","fox",100,100,500,"fire");
         FirePokemon fire3=new FirePokemon("Charizard","big","dragon",100,100,500,"fire");
         ArrayList<Pokemon> pokemonChoice = new ArrayList<>();
-
+        ArrayList<Pokemon> myPokemon=new ArrayList<>();
         pokemonChoice.add(fire1);
         pokemonChoice.add(fire2);
+        pokemonChoice.add(fire3);
 
         MenuLayout.drawFiles("pokemon_title.txt");
         System.out.println();
         Scanner in = new Scanner(System.in);
+
+
+
+        MenuLayout.choosePokemon(pokemonChoice,in,myPokemon);
+        for (int i = 0; i < myPokemon.size(); i++) {
+            System.out.println(myPokemon.get(i));
+
+        }
+
+
+
         menu.printMainMenu(in);
 
 
