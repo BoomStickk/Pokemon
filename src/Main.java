71ceeace0d6e76578773cb872e3ca3cca1b343a8
BoomStickk <6endgame6@gmail.com>
2022-11-29 02:28:37 +0200

    medling with the starting stats

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,16 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
 
-        Pokemon fire1 = new Pokemon("Charmender", "small", "lizard", 100, 20, 5000,"fire");
-        Pokemon fire2 = new Pokemon("Ninetails", "medium", "fox", 100, 20, 500,"fire");
-        Pokemon fire3 = new Pokemon("Charizard", "big", "dragon", 100, 20, 500,"fire");
+        Pokemon fire1 = new Pokemon("Charmender", "small", "lizard", 100, 20, 500,"fire");
+        Pokemon fire2 = new Pokemon("Ninetails", "medium", "fox", 150, 40, 600,"fire");
+        Pokemon fire3 = new Pokemon("Charizard", "big", "dragon", 200, 20, 700,"fire");
         Pokemon water1=new Pokemon("Squirtle","small","turtle",100,20,500,"water");
-        Pokemon water2=new Pokemon("Kecleon","medium","chameleon",100,20,500,"water");
-        Pokemon electric1=new Pokemon("Pikachu","small","mouse",500,20,4000,"electric");
-        Pokemon electric2=new Pokemon("Raichu","medium","mouse",100,20,500,"electric");
-        Pokemon rock1=new Pokemon("Sandshry","medium","armadillo",100,20,500,"rock");
-        Pokemon rock2=new Pokemon("Cubone","small","kangaroo",1000,75,500,"rock");
-        Pokemon rock3=new Pokemon("Tyranitar","big","dinosaur",100,20,500,"rock");
+        Pokemon water2=new Pokemon("Kecleon","medium","chameleon",150,40,600,"water");
+        Pokemon electric1=new Pokemon("Pikachu","small","mouse",100,20,500,"electric");
+        Pokemon electric2=new Pokemon("Raichu","medium","mouse",150,40,600,"electric");
+        Pokemon rock1=new Pokemon("Sandshry","medium","armadillo",150,40,600,"rock");
+        Pokemon rock2=new Pokemon("Cubone","small","kangaroo",100,20,500,"rock");
+        Pokemon rock3=new Pokemon("Tyranitar","big","dinosaur",200,60,700,"rock");
         ArrayList<Pokemon> pokemonChoice = new ArrayList<>();
         ArrayList<Pokemon> myPokemon = new ArrayList<>();
         ArrayList<Pokemon> enemyPokemon=new ArrayList<>();
